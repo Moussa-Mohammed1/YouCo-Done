832443124876cab774f7c1f273542a09d52fd08a
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-06 20:39:06 +0100

    fix(migration): change heureOuverture and heureFermeture fields to time type in horaires table

--- database/migrations/2026_02_02_144510_create_horaires_table.php
+++ database/migrations/2026_02_02_144510_create_horaires_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('horaires', function (Blueprint $table) {
             $table->id();
             $table->string('jourSemaine');
-            $table->dateTime('heureOuverture');
-            $table->dateTime('heureFermeture');
+            $table->time('heureOuverture');
+            $table->time('heureFermeture');
             $table->foreignId('restaurant_id')->constrained('restaurants')->cascadeOnDelete();
             $table->timestamps();
         });
